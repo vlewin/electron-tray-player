5487eb9d4518c2b6c54cf914673efbca2e04ec77
vlewin <vlewin@suse.de>
2016-12-04 17:08:23 +0100

    Add new 'player.fn.radio' module and mark radio.de as broken

---
+++ module_development.js
@@ -0,0 +1,6 @@
+var Module = require('./modules/iplayer.fm.radio.js')
+
+
+Module.load().then(function (playlist) {
+  console.log(playlist)
+})
---
+++ modules/iplayer.fm.radio.js
@@ -0,0 +1,40 @@
+var jsdom = require('jsdom')
+
+function load() {
+  return new Promise(function (resolve, reject) {
+    var url = 'http://iplayer.fm/radio/russia/'
+    var page = page || 1
+    var link = 'url' + '/' + page
+
+    jsdom.env({
+      url: url,
+      scripts: ['http://code.jquery.com/jquery.js'],
+      done: function (err, window) {
+        var list = []
+        var $ = window.$
+
+        console.log('my-hit.me', 'done')
+
+        $('.radio-station').each(function () {
+          var $item = $(this)
+          var $title = $item.find('h4')
+          var $img = $item.find('img')
+
+          list.push({
+            title: $title.html(),
+            stream: $item.data('mp3'),
+            image: 'http://iplayer.fm' + $img.attr('src')
+          })
+        })
+
+        console.log('iplayer.fm/radio', list)
+        resolve(list)
+      }
+    })
+
+  })
+}
+
+module.exports = {
+  load: load
+}
