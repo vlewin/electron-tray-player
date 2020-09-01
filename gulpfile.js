c0414ad4b28ac163f8d1daff9ca7a33ac2d8249a
Vladislav Lewin <vlewin@users.noreply.github.com>
2020-09-01 20:17:13 +0200

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,27 @@
 var gulp = require('gulp')
 var electron = require('electron-connect').server.create()
 
-gulp.task('default', ['serve'])
+// gulp.task('default', ['serve'])
 
-gulp.task('serve', function () {
-  console.log('watching')
+// gulp.task('serve', () => {
+//   console.log('watching')
 
-  // Start browser process
+//   // Start browser process
+//   electron.start()
+
+//   // Reload renderer process
+//   gulp.watch(['index.js', 'player.js', 'index.html', 'assets/css/*.css', 'modules/*.js'], electron.restart)
+
+//   // Reload renderer process
+//   // gulp.watch(['player.js', 'templates/index.tmpl'], electron.restart)
+// })
+
+gulp.task('serve', () => {
   electron.start()
-
-  // Reload renderer process
-  gulp.watch(['index.js', 'player.js', 'index.html', 'assets/css/*.css', 'modules/*.js'], electron.restart)
-
-  // Reload renderer process
-  // gulp.watch(['player.js', 'templates/index.tmpl'], electron.restart)
-})
+  gulp.watch(['index.js', 'player.js', 'index.html', 'assets/css/*.css', 'modules/*.js']).on('change', function(event) {
+      console.log(event.path);
+      console.log(event.type);
+      electron.restart
+      // code to execute
+  });
+});
